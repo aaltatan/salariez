ba22493eabfd6c5a9a8db2ecaba8ddb1e2a75b2d
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2024-09-20 19:14:18 +0300

    refactor deleter class

--- apps/base/utils/views.py
+++ apps/base/utils/views.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 from django.contrib import messages
+from django.http import HttpRequest
 from django.utils.translation import gettext_lazy as _
 
 
@@ -13,9 +14,17 @@ class Deleter(ABC):
     - `get_delete_message()` -> str  
     - `get_cannot_delete_message()` -> str  
     """
-    def __init__(self, instance, request) -> None:
-        self.request = request
+    def __init__(
+        self, 
+        instance, 
+        request: HttpRequest,
+        send_delete_messages: bool = True,
+        send_cannot_delete_messages: bool = True,
+    ) -> None:
         self.instance = instance
+        self.request = request
+        self.send_delete_messages = send_delete_messages
+        self.send_cannot_delete_messages = send_cannot_delete_messages
 
     def get_delete_message(self):
         return _('{} has been deleted successfully').format(self.instance)
@@ -24,14 +33,20 @@ class Deleter(ABC):
         return _('you can\'t delete this ({}) because there is one or more models related to it.').format(self.instance)
 
     def failure_scenario(self) -> None:
-        messages.error(self.request, self.get_cannot_delete_message())
+        if self.send_cannot_delete_messages:
+            messages.error(
+                self.request, self.get_cannot_delete_message()
+            )
 
     def success_scenario(self) -> None:
         self.instance.delete()
-        messages.success(self.request, self.get_delete_message())
+        if self.send_delete_messages:
+            messages.success(
+                self.request, self.get_delete_message()
+            )
 
     @abstractmethod
-    def can_delete_criteria(self):
+    def can_delete_criteria(self) -> bool:
         ...
 
     def delete(self) -> None:
