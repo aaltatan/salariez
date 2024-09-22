85f2a63fefebd743fc9699006157621e42b62960
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2024-09-22 21:05:40 +0300

    add employees application

--- apps/base/validators.py
+++ apps/base/validators.py
@@ -3,7 +3,11 @@ from django.utils.translation import gettext_lazy as _
 
 
 four_chars_validator = MinLengthValidator(
-    4, _('name of the faculty should not be less than 4 characters.')
+    4, _('name of the object should not be less than 4 characters.')
+)
+
+two_chars_validator = MinLengthValidator(
+    2, _('name of the object should not be less than 2 characters.')
 )
 
 numeric_validator = RegexValidator(
---
+++ apps/employees/models.py
@@ -0,0 +1,285 @@
+from django.db import models
+from django.db.models.fields.generated import GeneratedField
+from django.db.models.functions import Concat
+from django.conf import settings
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from apps.base.validators import (
+    two_chars_validator,
+    numeric_validator,
+)
+from apps.nationalities.models import Nationality
+from apps.areas.models import Area
+from apps.departments.models import Department
+from apps.job_subtypes.models import JobSubtype
+from apps.positions.models import Position
+from apps.statuses.models import Status
+
+
+class Employee(models.Model):
+    
+    class StatusChoices(models.TextChoices):
+        ACTIVE = 'A', _('active').title()
+        INACTIVE = 'I', _('inactive').title()
+    
+    class GenderChoices(models.TextChoices):
+        MALE = 'M', _('male').title()
+        FEMALE = 'F', _('female').title()
+    
+    class MartialStatusChoices(models.TextChoices):
+        MARRIED = 'M', _('married').title()
+        SINGLE = 'S', _('single').title()
+        DIVORCED = 'D', _('divorced').title()
+        OTHER = 'O', _('other').title()
+    
+    class MilitaryStatus(models.TextChoices):
+        DEFREMENTED = 'D', _('defremented').title()
+        FINISHED = 'F', _('finished').title()
+        EXCUSED = 'E', _('excused').title()
+        OTHER = 'O', _('other').title()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='employees'
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='employees'
+    )
+    firstname = models.CharField(
+        max_length=255,
+        verbose_name=_('first name'),
+        validators=[two_chars_validator]
+    )
+    lastname = models.CharField(
+        max_length=255,
+        verbose_name=_('last name'),
+        validators=[two_chars_validator]
+    )
+    father_name = models.CharField(
+        max_length=255,
+        verbose_name=_('father name'),
+        validators=[two_chars_validator]
+    )
+    fullname = GeneratedField(
+        expression=Concat(
+            models.F('firstname'),
+            models.Value(' '),
+            models.F('father_name'),
+            models.Value(' '),
+            models.F('last_name'),
+        ),
+        output_field=models.CharField(255),
+        db_persist=False,
+    )
+    search = GeneratedField(
+        expression=Concat(
+            models.F('firstname'),
+            models.Value(' '),
+            models.F('father_name'),
+            models.Value(' '),
+            models.F('last_name'),
+            models.Value(' '),
+            models.F('firstname'),
+            models.Value(' '),
+            models.F('father_name'),
+            models.Value(' '),
+            models.F('last_name'),
+        ),
+        output_field=models.CharField(500),
+        db_persist=False,
+    )
+    mother_name = models.CharField(
+        max_length=255,
+        verbose_name=_('mother name'),
+        validators=[two_chars_validator]
+    )
+    birth_place = models.CharField(
+        max_length=255,
+        verbose_name=_('birth place'),
+        validators=[two_chars_validator]
+    )
+    birth_date = models.DateField()
+    national_id = models.CharField(
+        max_length=255,
+        verbose_name=_('national id'),
+        unique=True,
+        validators=[numeric_validator]
+    )
+    card_id = models.CharField(
+        max_length=255,
+        verbose_name=_('card id'),
+        unique=True,
+        null=True,
+        validators=[numeric_validator]
+    )
+    # أمانة السجل المدني
+    civil_registry_office = models.CharField(
+        max_length=255,
+        default='',
+        blank=True,
+        verbose_name=_('civil registry office'),
+    )
+    # قيد السجل المدني
+    registry_office_name = models.CharField(
+        max_length=255,
+        default='',
+        blank=True,
+        verbose_name=_('registry office name'),
+    )
+    # رقم السجل المدني
+    registry_office_id = models.CharField(
+        max_length=255,
+        default='',
+        blank=True,
+        verbose_name=_('registry office id'),
+        validators=[numeric_validator]
+    )
+    gender = models.CharField(
+        max_length=6,
+        choices=GenderChoices.choices,
+        default=GenderChoices.MALE,
+    )
+    face_color = models.CharField(
+        max_length=255, default='', blank=True, verbose_name=_('face color')
+    )
+    eyes_color = models.CharField(
+        max_length=255, default='', blank=True, verbose_name=_('eyes color')
+    )
+    address = models.CharField(
+        max_length=255, default='', blank=True, verbose_name=_('address')
+    )
+    special_signs = models.CharField(
+        max_length=255,
+        default='',
+        blank=True,
+        verbose_name=_('special signs'),
+    )
+    card_date = models.DateField(null=True, blank=True)
+    martial_status = models.CharField(
+        max_length=6,
+        choices=MartialStatusChoices.choices,
+        default=MartialStatusChoices.SINGLE,
+    )
+    military_status = models.CharField(
+        max_length=6,
+        choices=MilitaryStatus.choices,
+        default=MilitaryStatus.EXCUSED,
+    )
+    nationality = models.ForeignKey(
+        Nationality, on_delete=models.PROTECT, related_name='employees'
+    )
+    area = models.ForeignKey(
+        Area, on_delete=models.PROTECT, related_name='employees'
+    )
+    department = models.ForeignKey(
+        Department, on_delete=models.PROTECT, related_name='employees'
+    )
+    position = models.ForeignKey(
+        Position, on_delete=models.PROTECT, related_name='employees'
+    )
+    job_subtype = models.ForeignKey(
+        JobSubtype, on_delete=models.PROTECT, related_name='employees'
+    )
+    job_status = models.ForeignKey(
+        Status, on_delete=models.PROTECT, related_name='employees'
+    )
+    status = models.CharField(
+        max_length=7,
+        choices=StatusChoices.choices,
+        default=StatusChoices.ACTIVE,
+    )
+    activate_date = models.DateField(blank=True, null=True)
+    deactivate_date = models.DateField(blank=True, null=True)
+    hire_date = models.DateField()
+    institution_id = models.CharField(
+        max_length=255,
+        verbose_name=_('institution id'),
+        unique=True,
+        null=True,
+        validators=[numeric_validator]
+    )
+    notes = models.TextField(
+        verbose_name=_('notes'),
+        max_length=1000,
+        default='',
+        blank=True
+    )
+    profile = models.ImageField(
+        upload_to='profiles', null=True, blank=True
+    )
+    identity_document = models.FileField(
+        upload_to='identities', null=True, blank=True
+    )
+    salary = models.DecimalField(
+        decimal_places=2, max_digits=12, default=0,
+    )
+
+    def _get_age(self, field: str) -> int:
+        today = timezone.now().date()
+        difference: timezone.timedelta = today - eval(f'self.{field}')
+        age = difference.days / 30 / 12
+        return round(age)
+
+    def _is_birthday_in_current_month(
+        self, field: str, step: int = 1
+    ) -> bool:
+        ...
+
+    def _is_birthday(self, field: str, step: int = 1) -> bool:
+        
+        today = timezone.now().date()
+
+        current_day, current_month = today.day, today.month
+
+        field_day = eval(f'self.{field}.day')
+        field_month = eval(f'self.{field}.month')
+
+        day_criteria = current_day == field_day
+        month_criteria = current_month == field_month
+
+        if step > 1:
+            current_year, field_year = today.year, eval(f'self.{field}.year')
+            year_diff = current_year - field_year
+            year_criteria = year_diff // step == 0
+            return day_criteria and month_criteria and year_criteria
+        
+        return day_criteria and month_criteria
+    
+    @property
+    def age(self) -> int:
+        self._get_age('birth_date')
+    
+    @property
+    def job_age(self) -> int:
+        self._get_age('hire_date')
+
+    @property
+    def is_birthday(self) -> bool:
+        return self._is_birthday('birth_date')
+
+    @property
+    def is_one_year_anniversary(self) -> bool:
+        return self._is_birthday('hire_date')
+
+    @property
+    def is_two_years_anniversary(self) -> bool:
+        return self._is_birthday('hire_date', 2)
+
+    def save(self, *args, **kwargs) -> None:
+        
+        if self.activate_date is None:
+            self.activate_date = timezone.now().date()
+        
+        if self.gender == self.GenderChoices.FEMALE.value:
+            self.military_status = self.MilitaryStatus.EXCUSED.value
+
+        return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return f'{self.firstname} {self.father_name} {self.lastname}'
