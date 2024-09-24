7e1393b62c907f733e32a574ead7ab3b1ef67c63
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2024-09-24 20:10:39 +0300

    fix apps classes names

--- apps/job_types/apps.py
+++ apps/job_types/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JobTypesConfig(AppConfig):
+class JobSubtypesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.job_types"
--- apps/nationalities/apps.py
+++ apps/nationalities/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NationalityConfig(AppConfig):
+class NationalitiesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.nationalities"
