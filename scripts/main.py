6adacd8ab2d96e82238eff7efcdf495b691bb11c
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2024-10-17 17:03:05 +0300

    create subquery script

--- scripts/main.py
+++ scripts/main.py
@@ -1,27 +1,88 @@
-from django.db.models import F, Q
-from django.db.models.functions import FirstValue
+from django.db.models import Subquery, OuterRef, Value
+from django.db.models.functions import Coalesce
 
 from icecream import ic
 
-from apps.employees.models import Employee
+from apps.employees.models import Employee, EducationTransaction
 
 
 def run() -> None:
    
-   qs = Employee.objects.annotate(
-      current_education=F('education_transactions__is_current'),
-      school=F('education_transactions__school__name'),
-      degree=F('education_transactions__degree__name'),
-      specialization=F('education_transactions__specialization__name'),
-      is_specialist=F('education_transactions__specialization__is_specialist'),
-   ).filter(Q(current_education=None) | Q(current_education=True))[:20]
+   transactions = (
+      EducationTransaction
+      .objects
+      .filter(is_current=True, employee=OuterRef('pk'))
+      .select_related('employee', 'specialization', 'degree', 'school')
+      .order_by('order')
+   )
+
+   qs = (
+      Employee
+      .objects
+      .annotate(
+         school=Coalesce(
+            Subquery(transactions.values('school__name')[:1]), Value('-')
+         ),
+         specialization=Coalesce(
+            Subquery(transactions.values('specialization__name')[:1]), Value('-')
+         ),
+         degree=Coalesce(
+            Subquery(transactions.values('degree__name')[:1]), Value('-')
+         ),
+         is_academic=Coalesce(
+            Subquery(transactions.values('degree__is_academic')[:1]), False
+         ),
+         is_specialist=Coalesce(
+            Subquery(transactions.values('specialization__is_specialist')[:1]), False
+         ),
+         graduation_date=Subquery(transactions.values('graduation_date')[:1]),
+      )
+   )[:20]
 
    for obj in qs:
       ic(
-         obj.fullname, 
-         obj.current_education, 
+         obj.fullname,
          obj.school,
-         obj.degree,
          obj.specialization,
+         obj.degree,
+         obj.is_academic,
+         obj.graduation_date,
          obj.is_specialist,
       )
+
+
+
+
+
+# def run() -> None:
+#    qs = Employee.objects.annotate(
+#       current_education=(
+#          Coalesce(
+#             F('education_transactions__is_current'), True
+#          )
+#       ),
+#       school=Coalesce(
+#          F('education_transactions__school__name'), Value('-')
+#       ),
+#       degree=Coalesce(
+#          F('education_transactions__degree__name'), Value('-')
+#       ),
+#       specialization=Coalesce(
+#          F('education_transactions__specialization__name'), Value('-')
+#       ),
+#       is_specialist=Coalesce(
+#          F('education_transactions__specialization__is_specialist'), 
+#          False,
+#       ),
+#    ).filter(current_education=True)[:20]
+
+
+#    for obj in qs:
+#       ic(
+#          obj.fullname, 
+#          obj.current_education, 
+#          obj.school,
+#          obj.degree,
+#          obj.specialization,
+#          obj.is_specialist,
+#       )
