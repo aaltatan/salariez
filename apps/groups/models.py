2b1a66f0d151bd07954376cf2ed61ba9d580fe73
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2024-10-22 23:23:56 +0300

    add get_absolute_path property in groups models refers to employees with filter

--- apps/groups/models.py
+++ apps/groups/models.py
@@ -1,9 +1,15 @@
+from django.urls import reverse
 from django.db.models.signals import pre_save
 
 from apps.base import models as base_models, utils
 
 
 class Group(base_models.AbstractNameModel):
+
+    @property
+    def get_absolute_path(self):
+        index_path = reverse('employees:index')
+        return f'{index_path}?groups={self.pk}'
 
     class Meta:
         ordering = ['name']
